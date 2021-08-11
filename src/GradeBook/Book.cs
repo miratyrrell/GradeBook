b1875c8739dda8447184252d3bcdfd5a41cf63fb
Miroslava Tyrrell <mira.tyrrell@gmail.com>
2021-08-11 15:15:02 +0200

    Introducing Letter Grades to the Gradebook via switch statement

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,6 +11,35 @@ namespace GradeBook
             this.name = name;
         }
 
+        public void AddLetterGrade (char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                case 'D':
+                    AddGrade(60);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+                    
+            }
+
+        }
+
+
         public void AddGrade(double grade)
         {
             //if statement is a Branching statement
@@ -43,6 +72,30 @@ namespace GradeBook
             }
 
             result.Average /= grades.Count;
+
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
